38003262b7ef92184dc137b0e9a70212ab71df09
majinpeng <2061363265@qq.com>
2020-10-18 18:56:47 +0800

    React任务二

---
+++ build/precache-manifest.0765d9c98c43af4b12ad36bdad62edba.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "240c7a95214fb52007b3d0d7f7b294d4",
+    "url": "./index.html"
+  },
+  {
+    "revision": "b7270822ba068a174255",
+    "url": "./static/css/2.f98a1de1.chunk.css"
+  },
+  {
+    "revision": "73c47a9d9347b204a249",
+    "url": "./static/css/main.45d0afe3.chunk.css"
+  },
+  {
+    "revision": "b7270822ba068a174255",
+    "url": "./static/js/2.3760d795.chunk.js"
+  },
+  {
+    "revision": "4d47ffde3dc69784e8e23fd910817ef8",
+    "url": "./static/js/2.3760d795.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "73c47a9d9347b204a249",
+    "url": "./static/js/main.7ae5ccc7.chunk.js"
+  },
+  {
+    "revision": "705f434324a29d64b932",
+    "url": "./static/js/runtime-main.075efe5b.js"
+  }
+]);
